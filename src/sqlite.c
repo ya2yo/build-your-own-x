b8b1ecd6ae7c76a20910c35feab54be7c6f8bc2e
Ya2yo <yayoy0015@gmail.com>
2026-08-30 15:11:13 +0800

    feat(sqlite): finish part5

--- src/sqlite.c
+++ src/sqlite.c
@@ -84,6 +84,7 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
+const uint32_t PAGER_SIZE = sizeof(Pager);
 const uint32_t TABLE_SIZE = sizeof(Table);
 
 /** Prints a row in the format used by the interactive shell. */
@@ -113,19 +114,76 @@ void deserial_row(void* source, Row* destination) {
 /* ================================ Pager ================================= */
 
 /** Opens the database file and initializes its pager and page cache. */
-Pager *pager_open(const char *filename) {}
+Pager *pager_open(const char *filename) {
+    int fd = open(filename, O_CREAT | O_RDWR, S_IWUSR | S_IRUSR);
+    if(fd == -1) {
+        printf("Unable to open %s", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    off_t file_length = lseek(fd, 0, SEEK_END);
+
+    Pager *pager = malloc(PAGER_SIZE);
+    pager->fd = fd;
+    pager->file_length = file_length;
+
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES;++i) {
+        pager->pages[i] = NULL;
+    }
+    return pager;
+}
 
 /** Returns the requested page, loading it into the pager cache if necessary. */
 void* get_page(Pager* pager, uint32_t page_num) {
-    if(page_num > TABLE_MAX_PAGES) {
-        printf("Tried to fetch page number out of bounds. %d > %d.\n", page_num,
+    if(page_num >= TABLE_MAX_PAGES) {
+        printf("Tried to fetch page number out of bounds. %d >= %d.\n", page_num,
                TABLE_MAX_PAGES);
         exit(EXIT_FAILURE);
     }
+    if(pager->pages[page_num]==NULL) {
+        // Cache miss. Allocate memory and load from file.
+        void *page = malloc(PAGE_SIZE);
+        uint32_t num_pages = (pager->file_length + PAGE_SIZE - 1) / PAGE_SIZE;
+
+        if (page_num < num_pages) {
+            // the data have been existed
+            lseek(pager->fd, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->fd, page, PAGE_SIZE);
+            if (bytes_read == -1) {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+        pager->pages[page_num] = page;
+    }
+    return pager->pages[page_num];
 }
 
 /** Writes a page from the in-memory cache back to the database file. */
-void page_flush(Pager *pager, uint32_t page_num, uint32_t size) {}
+void page_flush(Pager *pager, uint32_t page_num, uint32_t size) {
+    if(page_num >= TABLE_MAX_PAGES) {
+        printf("Tried to write page number out of bounds. %d >= %d.\n",
+               page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+    if(pager->pages[page_num] == NULL) {
+        printf("Tried to flush null page.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->fd, page_num * PAGE_SIZE, SEEK_SET);
+
+    if (offset == -1) {
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_written = write(pager->fd, pager->pages[page_num], size);
+    if(bytes_written==-1) {
+        printf("Error writing file: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
 
 /* ================================ Table ================================= */
 
@@ -151,7 +209,47 @@ void* row_slot(Table* table, uint32_t row_num) {
 
 /** Flushes cached pages and releases all resources owned by the table. */
 void db_close(Table* table) {
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->row_nums / ROWS_PER_PAGE;
 
+    for (uint32_t i = 0; i < num_full_pages;++i) {
+        if(pager->pages[i]==NULL) {
+            continue;
+        }
+        page_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+
+    // There may be a partial page to write to the end of the file
+    // This should not be needed after we switch to a B-tree
+    uint32_t num_additional_rows = table->row_nums % ROWS_PER_PAGE;
+    if(num_additional_rows>0) {
+        uint32_t page_num = num_full_pages;
+        if(pager->pages[page_num]!=NULL) {
+            page_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->fd);
+    if(result==-1) {
+        printf("Error closing db file.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // defendent code. Maybe could delete it
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES;++i) {
+        void *page = pager->pages[i];
+        if(page) {
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+
+    free(pager);
+    free(table);
 }
 
 /* ============================= Input buffer ============================== */
